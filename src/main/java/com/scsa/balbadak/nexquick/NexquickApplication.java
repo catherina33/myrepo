b42b28021f747c11944fafc47be69cc4e769c10c
이승진 <catherina.lee33@gmail.com>
2018-06-01 13:01:16 +0900

    VER 1.0

---
+++ src/main/java/com/scsa/balbadak/nexquick/NexquickApplication.java
@@ -0,0 +1,16 @@
+package com.scsa.balbadak.nexquick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexquickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexquickApplication.class, args);
+	}
+
+}
+
+
+
